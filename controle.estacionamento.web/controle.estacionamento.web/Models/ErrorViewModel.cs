ff502ffbc736b380b5778e913fb9e541e97cb408
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-06-14 20:44:22 -0300

    Primeiro Commit

---
+++ controle.estacionamento.web/controle.estacionamento.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace controle.estacionamento.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
