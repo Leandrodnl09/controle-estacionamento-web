956ed403548e7f36d9cda22cc8ae9a8428d00fed
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-06-15 11:23:47 -0300

    Implementação da tabela em Home

--- controle.estacionamento.web/controle.estacionamento.web/Models/CarrosModeloModel.cs
+++ controle.estacionamento.web/controle.estacionamento.web/Models/CarrosModeloModel.cs
@@ -1,6 +1,19 @@
-﻿namespace controle.estacionamento.web.Models
+﻿using System.Text.Json.Serialization;
+
+namespace controle.estacionamento.web.Models
 {
     public class CarrosModeloModel
     {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("marca")]
+        public string Marca { get; set; }
+
+        [JsonPropertyName("modelo")]
+        public string Modelo { get; set; }
+
+        [JsonPropertyName("ano")]
+        public int Ano { get; set;}
     }
 }
