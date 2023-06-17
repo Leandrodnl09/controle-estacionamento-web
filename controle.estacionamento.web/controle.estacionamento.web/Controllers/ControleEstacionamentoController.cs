f7ac07b33ed0c6bb4d0de32c4184a3e48a6222ed
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-06-17 17:22:32 -0300

    Iniciando Crud ControlePermanencia

---
+++ controle.estacionamento.web/controle.estacionamento.web/Controllers/ControleEstacionamentoController.cs
@@ -0,0 +1,54 @@
+﻿using controle.estacionamento.web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace controle.estacionamento.web.Controllers
+{
+    public class ControleEstacionamentoController : Controller
+    {
+        HttpClient _httpClient;
+
+        public ControleEstacionamentoController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7062");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var path = "api/controlepermanencia";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, path);
+
+            var response = await _httpClient.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var controlePermanencia = JsonSerializer.Deserialize<List<ControlePermanenciaCarrosModel>>(content);
+
+            return View(controlePermanencia);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(ControlePermanenciaCarrosModel model)
+        {
+            var path = "api/modelos";
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var messageRequest = new HttpRequestMessage(HttpMethod.Post, path)
+            {
+                Content = JsonContent.Create(model)
+            };
+
+            var response = await _httpClient.SendAsync(messageRequest);
+
+            response.EnsureSuccessStatusCode();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ controle.estacionamento.web/controle.estacionamento.web/Models/ControlePermanenciaCarrosModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace controle.estacionamento.web.Models
+{
+    public class ControlePermanenciaCarrosModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("placa")]
+        public string Placa { get; set; }
+        [JsonPropertyName("idModelo")]
+        public int IdModelo { get; set; }
+        [JsonPropertyName("dataHoraEntrada")]
+        public DateTime DataHoraEntrada { get; set; }
+        [JsonPropertyName("dataHoraSaida")]
+        public DateTime? DataHoraSaida { get; set; }
+    }
+}
